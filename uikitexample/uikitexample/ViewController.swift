d27898f404e4e182a6f360806018ad2f0b71cb92
mcgraths7 <mcgrath.steven7@gmail.com>
2018-08-14 09:43:20 -0400

    added all required ui elements

---
+++ uikitexample/uikitexample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  uikitexample
+//
+//  Created by Steven McGrath on 8/14/18.
+//  Copyright © 2018 Steven McGrath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
